4ec4c597883315b6df94bd2ccd650f808ecf8a30
maraosman94 <maraosman94@hotmail.com>
2023-02-12 12:29:35 -0600

    PLAYGROUND

--- gatsby/src/pages/playground.js
+++ gatsby/src/pages/playground.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useRef} from "react";
 import Layout from '../components/layout/layout'
 import { Seo } from "../components/layout/seo";
 import styled from 'styled-components'
+import p5 from "p5";
+
+// Aquí metes el archivo de tu sketch
+import sketch from "../sketches/sketch";
 
 const Container = styled.section`
 padding: 50px 50px 0 50px;
 height: 60vh;
 display: flex;
+flex-direction: column;
 justify-content: center;
 align-items: center;
 margin-bottom: 100px;
@@ -16,6 +21,12 @@ h1 {
     font-weight: 700;
 }
 
+.p5-js {
+  width: 50vw;
+  height: 50vh;
+  position: fixed;
+}
+
 @media (max-width: 890px) {
   padding: 50px 20px 0 20px;
 }
@@ -23,13 +34,14 @@ h1 {
 
 const PlaygroundPage = () => {
 
-
+  const sketchRef = useRef(sketch);
 
   return (
     <Layout pageTitle="Home Page">
       <Container>
         <h1>SOON</h1>
-        
+        <p>Aquí metes tus cosas</p>
+        <div className="p5-js" ref={{sketchRef}}></div>
       </Container>
     </Layout>
   )
---
+++ gatsby/src/sketches/sketch.js
@@ -0,0 +1,20 @@
+import p5 from 'p5';
+
+const s = (p) => {
+    let x = 100;
+    let y = 100;
+  
+    p.setup = function() {
+      p.createCanvas(700, 410);
+    };
+  
+    p.draw = function() {
+      p.background(0);
+      p.fill(255);
+      p.rect(p.mouseX, p.mouseY, 50, 50);
+    };
+  };
+
+  const sketch = new p5(s);
+
+  export default sketch;
